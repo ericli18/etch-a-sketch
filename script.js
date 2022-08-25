45cb138f56ba6b51708108a55ef570e9564a24f6
Eric Li <eric.li.8736@gmail.com>
2022-08-24 23:02:46 -0500

    Add color picker and fix layout

---
+++ script.js
@@ -0,0 +1,25 @@
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+    default: '#000000',
+    swatches: [
+        'rgba(244, 67, 54, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
