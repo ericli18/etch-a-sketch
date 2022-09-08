2e33fa2632e3d7cccc2fc2447f2cda7f20254262
Eric Li <eric.li.8736@gmail.com>
2022-09-08 11:30:19 -0500

    add div addition behavior

--- script.js
+++ script.js
@@ -23,11 +23,28 @@ const pickr = Pickr.create({
         }
     }
 });
-
 const slider = document.getElementById("slider");
 const sliderText = document.getElementById("slider-result");
+const container = document.querySelector(".sketcher");
 
 slider.oninput = function()
 {
     sliderText.textContent = slider.value;
+    updateDivs(slider.value);
+}
+
+function updateDivs(size)
+{
+    for(r = 0; r < size; r++)
+    {
+        for(c = 0; c < size; c++)
+        {
+            const tempBox = document.createElement('div');
+            container.append(tempBox);
+            tempBox.style.width = `${size / 500}px`;
+            tempBox.style.height = `${size / 500}px`;
+            tempBox.style.flex = "0 0 1";
+            tempBox.style.outline = "2px solid red";
+        }
+    }
 }
